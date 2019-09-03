7ad194e5a4a579f63a77fdb33a528513f6efd971
Deer5000 <khidrbrinkley32@gmail.com>
2019-09-03 00:16:01 -0700

    third commit

--- madlibs.py
+++ madlibs.py
@@ -33,6 +33,6 @@ def madlibs():
     adjective_6 = input('Enter an adjective: ')
     word_list.append(adjective_6)
 
-    print(" I walk through the tropical rainforest. I take out my {} flask. There's a {} Tiger with a {} in his mouth directly in front of me in the {} trees! I gaze at his {} {}. A sudden roar awakes me from my daydream! A panther {}'s in front of my face! I {} his {} breath. I remember I have a pocket full of {}'s that puts it to sleep! I {} it away from me in front of the {}. Yes he's ingesting it! I meet my brothers at the resort. Sheesh! Its been a thrilling day in the rainforest   ".format(adjective, adjective_1, noun_0, adjective_3, adjective_4, noun_1, verb, verb_1, adjective_5, noun_2, verb_2, noun_3, verb_3, adjective_6))
+    print(" I walk through the tropical rainforest. I take out my \033[1;32m {} \033[0m flask. There's a {} Tiger with a {} in his mouth directly in front of me in the {} trees! I gaze at his {} {}. A sudden roar awakes me from my daydream! A panther {}'s in front of my face! I {} his {} breath. I remember I have a pocket full of {}'s that puts it to sleep! I {} it away from me in front of the {}. Yes he's ingesting it! I meet my brothers at the resort. Sheesh! Its been a thrilling day in the rainforest   ".format(adjective, adjective_1, noun_0, adjective_3, adjective_4, noun_1, verb, verb_1, adjective_5, noun_2, verb_2, noun_3, verb_3, adjective_6))
 #random.shuffle(adjective_1,adjective_3,adjective_5)
 madlibs()
